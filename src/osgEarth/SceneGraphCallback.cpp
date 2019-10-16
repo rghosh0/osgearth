48f2eb795c9818cdfafca1e92a09ccd94625cee7
Benjamin SENEGAS <benjamin.senegas@capgemini.com>
2019-10-16 17:10:14 +0200

    Add max_visibility_altitude for layout visibility on earth file

--- src/osgEarth/SceneGraphCallback.cpp
+++ src/osgEarth/SceneGraphCallback.cpp
@@ -1,162 +1,221 @@
 /* -*-c++-*- */
 /* osgEarth - Geospatial SDK for OpenSceneGraph
-* Copyright 2019 Pelican Mapping
-* http://osgearth.org
-*
-* osgEarth is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-* IN THE SOFTWARE.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
+ * Copyright 2019 Pelican Mapping
+ * http://osgearth.org
+ *
+ * osgEarth is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+#include <osgEarth/CullingUtils>
 #include <osgEarth/SceneGraphCallback>
+#include <osgUtil/CullVisitor>
 
 using namespace osgEarth;
 
-//...................................................................
-
-SceneGraphCallbacks::SceneGraphCallbacks(osg::Object* sender) :
-_sender(sender)
-{
-    //nop
+PagedLODwithVisibilityAltitude::PagedLODwithVisibilityAltitude()
+    : _visibilityMaxAltitude(FLT_MAX), _currentAltitude(FLT_MAX),
+      _isVisible(true) {
+  // nop
 }
 
-void
-SceneGraphCallbacks::add(SceneGraphCallback* cb)
-{
-    if (cb)
-    {
-        Threading::ScopedMutexLock lock(_mutex);
-        _callbacks.push_back(cb);
+void PagedLODwithVisibilityAltitude::setVisibilityMaxAltitude(
+    float visibilityMaxAltitude) {
+  _visibilityMaxAltitude = visibilityMaxAltitude;
+
+  // dirty the camera altitude so as to force a new cull computation
+  _currentAltitude = FLT_MAX;
+}
+
+bool PagedLODwithVisibilityAltitude::addChild(osg::Node *child) {
+  bool ok = false;
+  if (child) {
+    ok = osg::PagedLOD::addChild(child);
+    if (ok) {
+      // dirty the camera altitude so as to force a new cull computation
+      _currentAltitude = FLT_MAX;
     }
+  }
+  return ok;
 }
 
-void
-SceneGraphCallbacks::remove(SceneGraphCallback* cb)
-{
-    if (cb)
-    {
-        Threading::ScopedMutexLock lock(_mutex);
-        for (SceneGraphCallbackVector::iterator i = _callbacks.begin(); i != _callbacks.end(); ++i)
-        {
-            if (i->get() == cb)
-            {
-                _callbacks.erase(i);
-                break;
-            }
+bool PagedLODwithVisibilityAltitude::insertChild(unsigned index,
+                                                 osg::Node *child) {
+  bool ok = false;
+  if (child) {
+    ok = osg::PagedLOD::insertChild(index, child);
+    if (ok) {
+      // dirty the camera altitude so as to force a new cull computation
+      _currentAltitude = FLT_MAX;
+    }
+  }
+  return ok;
+}
+
+bool PagedLODwithVisibilityAltitude::replaceChild(osg::Node *oldChild,
+                                                  osg::Node *newChild) {
+  bool ok = false;
+  if (oldChild && newChild) {
+    ok = osg::PagedLOD::replaceChild(oldChild, newChild);
+    if (ok) {
+      // dirty the camera altitude so as to force a new cull computation
+      _currentAltitude = FLT_MAX;
+    }
+  }
+  return ok;
+}
+
+void PagedLODwithVisibilityAltitude::traverse(osg::NodeVisitor &nv) {
+  if (_visibilityMaxAltitude != FLT_MAX &&
+      nv.getVisitorType() == osg::NodeVisitor::CULL_VISITOR) {
+    osgUtil::CullVisitor *cv = Culling::asCullVisitor(nv);
+    osg::Camera *camera = cv->getCurrentCamera();
+
+    // work only on reference camera
+    if (camera && !camera->isRenderToTextureCamera()) {
+      double alt;
+      if (camera->getUserValue("altitude", alt) && _currentAltitude != alt) {
+        _currentAltitude = alt;
+        if (_currentAltitude > _visibilityMaxAltitude) {
+          if (_isVisible && getNumChildren() > 0) {
+            getChild(0)->setNodeMask(0);
+            _isVisible = false;
+          }
+        } else {
+          if (!_isVisible && getNumChildren() > 0) {
+            getChild(0)->setNodeMask(~0);
+            _isVisible = true;
+          }
         }
+      }
     }
-}
+  }
 
-void
-SceneGraphCallbacks::firePreMergeNode(osg::Node* node)
-{
-    Threading::ScopedMutexLock lock(_mutex);
-    osg::ref_ptr<osg::Object> sender;
-    _sender.lock(sender);
-    for (SceneGraphCallbackVector::iterator i = _callbacks.begin(); i != _callbacks.end(); ++i)
-        i->get()->onPreMergeNode(node, sender.get());
-}
-
-void
-SceneGraphCallbacks::firePostMergeNode(osg::Node* node)
-{
-    osg::ref_ptr<osg::Object> sender;
-    _sender.lock(sender);
-    for (SceneGraphCallbackVector::iterator i = _callbacks.begin(); i != _callbacks.end(); ++i)
-        i->get()->onPostMergeNode(node, sender.get());
-}
-
-void
-SceneGraphCallbacks::fireRemoveNode(osg::Node* node)
-{
-    osg::ref_ptr<osg::Object> sender;
-    _sender.lock(sender);
-    for (SceneGraphCallbackVector::iterator i = _callbacks.begin(); i != _callbacks.end(); ++i)
-        i->get()->onRemoveNode(node, sender.get());
+  osg::PagedLOD::traverse(nv);
 }
 
 //...................................................................
 
-PagedLODWithSceneGraphCallbacks::PagedLODWithSceneGraphCallbacks(SceneGraphCallbacks* host) :
-_host(host)
-{
-    //nop
+SceneGraphCallbacks::SceneGraphCallbacks(osg::Object *sender)
+    : _sender(sender) {
+  // nop
 }
 
-SceneGraphCallbacks*
-PagedLODWithSceneGraphCallbacks::getSceneGraphCallbacks() const
-{
-    return _host.get();
+void SceneGraphCallbacks::add(SceneGraphCallback *cb) {
+  if (cb) {
+    Threading::ScopedMutexLock lock(_mutex);
+    _callbacks.push_back(cb);
+  }
 }
 
-void
-PagedLODWithSceneGraphCallbacks::setSceneGraphCallbacks(SceneGraphCallbacks* host)
-{
-    _host = host;
-}
-
-bool
-PagedLODWithSceneGraphCallbacks::addChild(osg::Node* child)
-{
-    bool ok = false;
-    if (child)
-    {
-        ok = osg::PagedLOD::addChild(child);
-        osg::ref_ptr<SceneGraphCallbacks> host;
-        if (_host.lock(host))
-            host->firePostMergeNode(child);
+void SceneGraphCallbacks::remove(SceneGraphCallback *cb) {
+  if (cb) {
+    Threading::ScopedMutexLock lock(_mutex);
+    for (SceneGraphCallbackVector::iterator i = _callbacks.begin();
+         i != _callbacks.end(); ++i) {
+      if (i->get() == cb) {
+        _callbacks.erase(i);
+        break;
+      }
     }
-    return ok;
+  }
 }
 
-bool
-PagedLODWithSceneGraphCallbacks::insertChild(unsigned index, osg::Node* child)
-{
-    bool ok = false;
-    if (child)
-    {
-        ok = osg::PagedLOD::insertChild(index, child);
-        osg::ref_ptr<SceneGraphCallbacks> host;
-        if (_host.lock(host))
-            host->firePostMergeNode(child);
-    }
-    return ok;
+void SceneGraphCallbacks::firePreMergeNode(osg::Node *node) {
+  Threading::ScopedMutexLock lock(_mutex);
+  osg::ref_ptr<osg::Object> sender;
+  _sender.lock(sender);
+  for (SceneGraphCallbackVector::iterator i = _callbacks.begin();
+       i != _callbacks.end(); ++i)
+    i->get()->onPreMergeNode(node, sender.get());
 }
 
-bool
-PagedLODWithSceneGraphCallbacks::replaceChild(osg::Node* oldChild, osg::Node* newChild)
-{
-    bool ok = false;
-    if (oldChild && newChild)
-    {
-        ok = osg::PagedLOD::replaceChild(oldChild, newChild);
-        osg::ref_ptr<SceneGraphCallbacks> host;
-        if (_host.lock(host))
-            host->firePostMergeNode(newChild);
-    }
-    return ok;
+void SceneGraphCallbacks::firePostMergeNode(osg::Node *node) {
+  osg::ref_ptr<osg::Object> sender;
+  _sender.lock(sender);
+  for (SceneGraphCallbackVector::iterator i = _callbacks.begin();
+       i != _callbacks.end(); ++i)
+    i->get()->onPostMergeNode(node, sender.get());
 }
 
-void
-PagedLODWithSceneGraphCallbacks::childRemoved(unsigned pos, unsigned num)
-{
+void SceneGraphCallbacks::fireRemoveNode(osg::Node *node) {
+  osg::ref_ptr<osg::Object> sender;
+  _sender.lock(sender);
+  for (SceneGraphCallbackVector::iterator i = _callbacks.begin();
+       i != _callbacks.end(); ++i)
+    i->get()->onRemoveNode(node, sender.get());
+}
+
+//...................................................................
+
+PagedLODWithSceneGraphCallbacks::PagedLODWithSceneGraphCallbacks(
+    SceneGraphCallbacks *host)
+    : PagedLODwithVisibilityAltitude(), _host(host) {
+  // nop
+}
+
+SceneGraphCallbacks *
+PagedLODWithSceneGraphCallbacks::getSceneGraphCallbacks() const {
+  return _host.get();
+}
+
+void PagedLODWithSceneGraphCallbacks::setSceneGraphCallbacks(
+    SceneGraphCallbacks *host) {
+  _host = host;
+}
+
+bool PagedLODWithSceneGraphCallbacks::addChild(osg::Node *child) {
+  bool ok = false;
+  if (child) {
+    ok = PagedLODwithVisibilityAltitude::addChild(child);
     osg::ref_ptr<SceneGraphCallbacks> host;
     if (_host.lock(host))
-    {
-        for (unsigned i = pos; i < pos + num; ++i)
-        {
-            host->fireRemoveNode(getChild(i));
-        }
+      host->firePostMergeNode(child);
+  }
+  return ok;
+}
+
+bool PagedLODWithSceneGraphCallbacks::insertChild(unsigned index,
+                                                  osg::Node *child) {
+  bool ok = false;
+  if (child) {
+    ok = PagedLODwithVisibilityAltitude::insertChild(index, child);
+    osg::ref_ptr<SceneGraphCallbacks> host;
+    if (_host.lock(host))
+      host->firePostMergeNode(child);
+  }
+  return ok;
+}
+
+bool PagedLODWithSceneGraphCallbacks::replaceChild(osg::Node *oldChild,
+                                                   osg::Node *newChild) {
+  bool ok = false;
+  if (oldChild && newChild) {
+    ok = PagedLODwithVisibilityAltitude::replaceChild(oldChild, newChild);
+    osg::ref_ptr<SceneGraphCallbacks> host;
+    if (_host.lock(host))
+      host->firePostMergeNode(newChild);
+  }
+  return ok;
+}
+
+void PagedLODWithSceneGraphCallbacks::childRemoved(unsigned pos, unsigned num) {
+  osg::ref_ptr<SceneGraphCallbacks> host;
+  if (_host.lock(host)) {
+    for (unsigned i = pos; i < pos + num; ++i) {
+      host->fireRemoveNode(getChild(i));
     }
+  }
 }
--- src/osgEarthFeatures/FeatureDisplayLayout.cpp
+++ src/osgEarthFeatures/FeatureDisplayLayout.cpp
@@ -16,8 +16,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
-#include <osgEarthFeatures/FeatureDisplayLayout>
 #include <limits>
+#include <osgEarthFeatures/FeatureDisplayLayout>
 
 using namespace osgEarth;
 using namespace osgEarth::Features;
@@ -25,131 +25,105 @@ using namespace osgEarth::Symbology;
 
 //------------------------------------------------------------------------
 
-FeatureLevel::FeatureLevel( const Config& conf ) :
-_minRange( 0.0f ),
-_maxRange( FLT_MAX )
-{
-    fromConfig( conf );
+FeatureLevel::FeatureLevel(const Config &conf)
+    : _minRange(0.0f), _maxRange(FLT_MAX), _maxVisibilityAltitude(FLT_MAX) {
+  fromConfig(conf);
 }
 
-FeatureLevel::FeatureLevel( float minRange, float maxRange )
-{
-    _minRange = minRange;
-    _maxRange = maxRange;
+FeatureLevel::FeatureLevel(float minRange, float maxRange) {
+  _minRange = minRange;
+  _maxRange = maxRange;
 }
 
-FeatureLevel::FeatureLevel( float minRange, float maxRange, const std::string& styleName )
-{
-    _minRange = minRange;
-    _maxRange = maxRange;
-    _styleName = styleName;
+FeatureLevel::FeatureLevel(float minRange, float maxRange,
+                           const std::string &styleName) {
+  _minRange = minRange;
+  _maxRange = maxRange;
+  _styleName = styleName;
 }
 
-void
-FeatureLevel::fromConfig( const Config& conf )
-{
-    conf.get( "min_range", _minRange );
-    conf.get( "max_range", _maxRange );
-    conf.get( "style",     _styleName ); 
-    conf.get( "class",     _styleName ); // alias
+void FeatureLevel::fromConfig(const Config &conf) {
+  conf.get("min_range", _minRange);
+  conf.get("max_range", _maxRange);
+  conf.get("max_visibility_altitude", _maxVisibilityAltitude);
+  conf.get("style", _styleName);
+  conf.get("class", _styleName); // alias
 }
 
-Config
-FeatureLevel::getConfig() const
-{
-    Config conf( "level" );
-    conf.set( "min_range", _minRange );
-    conf.set( "max_range", _maxRange );
-    conf.set( "style",     _styleName );
-    return conf;
+Config FeatureLevel::getConfig() const {
+  Config conf("level");
+  conf.set("min_range", _minRange);
+  conf.set("max_range", _maxRange);
+  conf.set("max_visibility_altitude", _maxVisibilityAltitude);
+  conf.set("style", _styleName);
+  return conf;
 }
 
 //------------------------------------------------------------------------
 
-FeatureDisplayLayout::FeatureDisplayLayout( const Config& conf ) :
-_tileSizeFactor( 15.0f ),
-_minRange      ( 0.0f ),
-_maxRange      ( 0.0f ),
-_cropFeatures  ( false ),
-_priorityOffset( 0.0f ),
-_priorityScale ( 1.0f ),
-_minExpiryTime ( 0.0f ),
-_paged(true)
-{
-    fromConfig( conf );
+FeatureDisplayLayout::FeatureDisplayLayout(const Config &conf)
+    : _tileSizeFactor(15.0f), _minRange(0.0f), _maxRange(0.0f),
+      _cropFeatures(false), _priorityOffset(0.0f), _priorityScale(1.0f),
+      _minExpiryTime(0.0f), _paged(true) {
+  fromConfig(conf);
 }
 
-void
-FeatureDisplayLayout::fromConfig( const Config& conf )
-{
-    conf.get( "tile_size",        _tileSize );
-    conf.get( "tile_size_factor", _tileSizeFactor );
-    conf.get( "crop_features",    _cropFeatures );
-    conf.get( "priority_offset",  _priorityOffset );
-    conf.get( "priority_scale",   _priorityScale );
-    conf.get( "min_expiry_time",  _minExpiryTime );
-    conf.get( "min_range",        _minRange );
-    conf.get( "max_range",        _maxRange );
-    conf.get("paged", _paged);
-    ConfigSet children = conf.children( "level" );
-    for( ConfigSet::const_iterator i = children.begin(); i != children.end(); ++i )
-        addLevel( FeatureLevel( *i ) );
+void FeatureDisplayLayout::fromConfig(const Config &conf) {
+  conf.get("tile_size", _tileSize);
+  conf.get("tile_size_factor", _tileSizeFactor);
+  conf.get("crop_features", _cropFeatures);
+  conf.get("priority_offset", _priorityOffset);
+  conf.get("priority_scale", _priorityScale);
+  conf.get("min_expiry_time", _minExpiryTime);
+  conf.get("min_range", _minRange);
+  conf.get("max_range", _maxRange);
+  conf.get("paged", _paged);
+  ConfigSet children = conf.children("level");
+  for (ConfigSet::const_iterator i = children.begin(); i != children.end(); ++i)
+    addLevel(FeatureLevel(*i));
 }
 
-Config
-FeatureDisplayLayout::getConfig() const
-{
-    Config conf( "layout" );
-    conf.set( "tile_size",        _tileSize );
-    conf.set( "tile_size_factor", _tileSizeFactor );
-    conf.set( "crop_features",    _cropFeatures );
-    conf.set( "priority_offset",  _priorityOffset );
-    conf.set( "priority_scale",   _priorityScale );
-    conf.set( "min_expiry_time",  _minExpiryTime );
-    conf.set( "min_range",        _minRange );
-    conf.set( "max_range",        _maxRange );
-    conf.set("paged", _paged);
-    for( Levels::const_iterator i = _levels.begin(); i != _levels.end(); ++i )
-        conf.add( i->second.getConfig() );
-    return conf;
+Config FeatureDisplayLayout::getConfig() const {
+  Config conf("layout");
+  conf.set("tile_size", _tileSize);
+  conf.set("tile_size_factor", _tileSizeFactor);
+  conf.set("crop_features", _cropFeatures);
+  conf.set("priority_offset", _priorityOffset);
+  conf.set("priority_scale", _priorityScale);
+  conf.set("min_expiry_time", _minExpiryTime);
+  conf.set("min_range", _minRange);
+  conf.set("max_range", _maxRange);
+  conf.set("paged", _paged);
+  for (Levels::const_iterator i = _levels.begin(); i != _levels.end(); ++i)
+    conf.add(i->second.getConfig());
+  return conf;
 }
 
-void 
-FeatureDisplayLayout::addLevel( const FeatureLevel& level )
-{
-    _levels.insert( std::make_pair( -level.maxRange().get(), level ) );
+void FeatureDisplayLayout::addLevel(const FeatureLevel &level) {
+  _levels.insert(std::make_pair(-level.maxRange().get(), level));
 }
 
-unsigned 
-FeatureDisplayLayout::getNumLevels() const
-{
-    return _levels.size();
+unsigned FeatureDisplayLayout::getNumLevels() const { return _levels.size(); }
+
+const FeatureLevel *FeatureDisplayLayout::getLevel(unsigned n) const {
+  unsigned i = 0;
+  for (Levels::const_iterator k = _levels.begin(); k != _levels.end(); ++k) {
+    if (n == i++)
+      return &(k->second);
+  }
+  return 0L;
 }
 
-const FeatureLevel*
-FeatureDisplayLayout::getLevel( unsigned n ) const
-{
-    unsigned i = 0;
-    for( Levels::const_iterator k = _levels.begin(); k != _levels.end(); ++k )
-    {
-        if ( n == i++ )
-            return &(k->second);
-    }
-    return 0L;
-}
+unsigned FeatureDisplayLayout::chooseLOD(const FeatureLevel &level,
+                                         double fullExtentRadius) const {
+  double radius = fullExtentRadius;
+  unsigned lod = 1;
+  for (; lod < 20; ++lod) {
+    radius *= 0.5;
+    float lodMaxRange = radius * _tileSizeFactor.value();
 
-unsigned
-FeatureDisplayLayout::chooseLOD( const FeatureLevel& level, double fullExtentRadius ) const
-{
-    double radius = fullExtentRadius;    
-    unsigned lod = 1;
-    for( ; lod < 20; ++lod )
-    {
-        radius *= 0.5;
-        float lodMaxRange = radius * _tileSizeFactor.value();
-        
-        if ( level.maxRange() >= lodMaxRange )
-            break;
-    }
-    return lod-1;
+    if (level.maxRange() >= lodMaxRange)
+      break;
+  }
+  return lod - 1;
 }
