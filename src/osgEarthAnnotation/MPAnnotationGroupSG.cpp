ad91446042bb5989d7e24bbf1ae17ef982bd3204
MPcoreDev <52664431+MPcoreDev@users.noreply.github.com>
2020-04-23 09:30:24 +0200

    MIS-1888 Fix include

--- src/osgEarthAnnotation/MPAnnotationGroupSG.cpp
+++ src/osgEarthAnnotation/MPAnnotationGroupSG.cpp
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-#include <osgEarthAnnotation/MPAnnotationGroupSG.>
+#include <osgEarthAnnotation/MPAnnotationGroupSG>
 #include <osgEarthAnnotation/MPAnnotationDrawable>
 #include <osgEarthAnnotation/AnnotationUtils>
 #include <osgEarth/VirtualProgram>
